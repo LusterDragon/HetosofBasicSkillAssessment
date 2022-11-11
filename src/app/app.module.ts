919b914c063371b841134040bc471250a24c2252
LusterDragon <procyon.iotor@protonmail.com>
2022-11-11 13:43:53 -0300

    add routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { TesteComponent } from './pages/teste/teste.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HetosoftService } from './services/hetosoft-service.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    TesteComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    HetosoftService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
